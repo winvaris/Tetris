72e6713b74a14bf77dd73e5d4c70a9e6cab25e6d
Varis <win3455@hotmail.com>
2017-04-30 02:16:52 +0700

    Add GameController script

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private int[,] blocks;
+
+	// Use this for initialization
+	void Awake () {
+		blocks = new int [10, 20];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
