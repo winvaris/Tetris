0abbbdd195570b7c193c25b98af4b09175a378e8
MindaRyn <natcha.p@ku.th>
2017-05-03 01:00:20 +0700

    Add button event

---
+++ Assets/Scripts/ButtonEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEvent : MonoBehaviour {
+	private Image myImage;
+	public Sprite normal, hover, clicked;
+
+	// Use this for initialization
+	void Start () {
+		myImage = gameObject.GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ButtonHover() {
+		myImage.sprite = hover;
+	}
+
+	public void ButtonExit() {
+		myImage.sprite = normal;
+	}
+
+	public void ButtonDown() {
+		myImage.sprite = clicked;
+	}
+
+	public void ButtonUp() {
+		myImage.sprite = hover;
+	}
+}
