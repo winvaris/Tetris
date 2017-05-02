1685a61468d545b6ac4e83c59c25b1ce8249d7bb
Varis <win3455@hotmail.com>
2017-05-02 20:37:28 +0700

    Created Tetromino

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	private int[,] blocks;
+	private GameObject tetrominoObj;
+	private TetrominoController tetromino;
+	private bool hasPlaced;
+	private bool gameRunning;
+	public float dropDelay;
+	private float dropDelayCounter;
+	private int currentX;
+	private int currentY;
 
 	// Use this for initialization
 	void Awake () {
-		blocks = new int [10, 20];
+		blocks = new int [20, 10];
+		hasPlaced = false;
+		gameRunning = false;
+		dropDelay = 1f;
+		dropDelayCounter = dropDelay;
+		ResetCurrentPos ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!gameRunning) {
+			return;
+		}
+
+		if (dropDelayCounter > 0) {
+			dropDelayCounter -= Time.deltaTime;
+		}
+	}
+
+	private void ResetCurrentPos () {
+		currentX = 4;
+		currentY = 1;
 	}
 }
